cc70fe6807a9517769c029546087d358f715d84f
kancy <793272861@qq.com>
2021-11-21 21:59:25 +0800

    优化组件

--- sfer/tests/aes_tests.py
+++ sfer/tests/aes_tests.py
@@ -10,4 +10,5 @@ print(f"{data} - {enc}")
 password = "1234567812345678"
 a = sfer.Aes(password)
 print(a.encrypt("test"))
+print(a.decrypt(a.encrypt("test")))
 print(a.decrypt(a.encrypt("test"), password))
