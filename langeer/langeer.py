0e2f715147aea29c29ec63dbd714e058dfcf87c5
kancy <793272861@qq.com>
2021-11-29 22:43:50 +0800

    优化

--- langeer/langeer.py
+++ langeer/langeer.py
@@ -34,6 +34,10 @@ def isArray(value) -> bool:
     return isTuple(value)
 
 
+def isByteArray(value) -> bool:
+    return isinstance(value, (bytes, bytearray))
+
+
 def isCollection(value) -> bool:
     return isinstance(value, (list, set, tuple))
 
@@ -44,6 +48,42 @@ def isDict(value) -> bool:
 
 def isMap(value) -> bool:
     return isDict(value)
+
+
+def isClass(value) -> bool:
+    return type(value).__name__ == 'type' and str(value).startswith("<class '")
+
+
+def isBaseType(value) -> bool:
+    return isinstance(value, (int, float, bool, str, dict, tuple, list, set, bytes, bytearray, complex))
+
+
+def getClassName(value) -> str:
+    if isClass(value):
+        return value.__name__
+    else:
+        return type(value).__name__
+
+
+def findClass(className):
+    return forClass(className)
+
+
+def forClass(className):
+    import sys
+    cls = None
+    stop = False
+    f = sys._getframe()
+    while not stop:
+        if f.f_globals.__contains__(className):
+            c = f.f_globals[className]
+            if isClass(c):
+                cls = c
+                break
+        if f.f_globals['__name__'] == '__main__':
+            stop = True
+        f = f.f_back
+    return cls
 
 
 def isNull(obj) -> bool:
@@ -143,3 +183,41 @@ def assertBlank(obj, message=None):
 def assertNotBlank(obj, message=None):
     if isBlank(obj):
         raise AssertionError(message or "assert not blank, but value is blank")
+
+
+def obj2dict(obj, recursive=True):
+    """把Object对象转换成Dict对象"""
+    try:
+        obj_dict = obj.__dict__
+    except:
+        return obj
+    ret_obj_dict = {}
+    ret_obj_dict.update(obj_dict)
+    if recursive:
+        for field in obj_dict:
+            fieldValue = obj_dict[field]
+            if not isBaseType(fieldValue):
+                ret_obj_dict[field] = obj2dict(fieldValue)
+    return ret_obj_dict
+
+
+def dict2obj(mapping: dict, obj):
+    """
+    将字段的属性赋值给对象
+    :param mapping: 字典
+    :param obj: 对象实例、类型、类名
+    :return:
+    """
+    if isClass(obj):
+        obj = obj()
+    elif isinstance(obj, str):
+        objClass = forClass(obj)
+        if objClass:
+            obj = objClass()
+        else:
+            raise RuntimeError(f"Not Found class : {obj}")
+    try:
+        obj.__dict__.update(mapping)
+    except:
+        pass
+    return obj
--- langeer/tests/langeer_tests.py
+++ langeer/tests/langeer_tests.py
@@ -1,4 +1,34 @@
-import langeer.langeer as l
+import datetime
+import decimal
 
-from fileer import *
+
+class A(object):
+
+    def __init__(self) -> None:
+        self.name = 'string'
+        self.age = 18
+        self.score = decimal.Decimal('85.5')
+        self.love = {'apple', 'cat', 'dog'}
+        self.logs = ['日志1', '日志2']
+
+
+class B(A):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.a = A()
+        self.date = datetime.datetime.now()
+        self.datetime = datetime.date.today()
+
+
+class C(object):
+
+    def __init__(self) -> None:
+        self.a = A()
+        self.b = B()
+
+
+import langeer
+
+print(langeer.obj2dict(C()))
 
